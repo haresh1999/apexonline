6088c8490ccb8d7e872004ce3421c5ebe0c36b11
Haresh Chauhan <hareshc1999@gmail.com>
2026-06-30 18:16:38 +0530

    update

--- resources/views/admin/transaction/list.blade.php
+++ resources/views/admin/transaction/list.blade.php
@@ -43,7 +43,7 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('tnx.index',['status' => 'pencing']) }}">
+            <a class="nav-link" href="{{ route('tnx.index',['status' => 'pending']) }}">
                 <span>Pending</span>
                 <span class="text-body-tertiary fw-semibold">({{$pendingTotal}})</span>
             </a>
