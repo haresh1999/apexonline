46c034a030902017e32382b8f0e81d7efbbc532d
Haresh Chauhan <hareshc1999@gmail.com>
2026-06-30 17:49:42 +0530

    update

--- app/Models/Gateway.php
+++ app/Models/Gateway.php
@@ -14,6 +14,6 @@ class Gateway extends Model
 
     public function transactions()
     {
-        return $this->hasMany(Transaction::class, 'gateway_id')->where('env', 'production');
+        return $this->hasMany(Transaction::class, 'gateway_id')->where('env', 'production')->where('status', 'completed');
     }
 }
