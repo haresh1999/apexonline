b4235092340a919bae44987eea1e65ecb91aff63
Haresh Chauhan <hareshc1999@gmail.com>
2026-09-15 12:46:17 +0530

    update

--- resources/views/admin/webhook/list.blade.php
+++ resources/views/admin/webhook/list.blade.php
@@ -84,7 +84,7 @@
     <div class="modal-dialog modal-xl modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="payment-info">#{{ids($log->tnx_id)}} Payment Info!</h5>
+                <h5 class="modal-title" id="payment-info">#{{$log->tnx_id}} Payment Info!</h5>
                 <button class="btn btn-close p-1" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
